77f144c2f858f81d7f4f1f48959354d343ac1908
drizio <driziob@gmail.com>
2020-12-21 11:53:40 +0100

    Created src folder for the app

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Root from './src/Root.js'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+  return <View style={styles.container}>
+      <Root />
       <StatusBar style="auto" />
     </View>
-  );
 }
 
 const styles = StyleSheet.create({
